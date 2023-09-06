41a4244b433c3fc5d6b8040f654ced2afc11b09f
Manas Tiwari <manastiwari002@gmail.com>
2023-09-06 15:29:30 +0530

    created a good looking website with react and tailwind

---
+++ src/assets/images/index.js
@@ -0,0 +1,40 @@
+import bigShoe1 from './big-shoe1.png'
+import bigShoe2 from './big-shoe2.png'
+import bigShoe3 from './big-shoe3.png'
+
+import customer1 from './customer1.jpeg'
+import customer2 from './customer2.svg'
+
+import footerLogo from './footer-logo.png'
+import headerLogo from './header-logo.svg'
+
+import offer from './offer.jpg'
+
+import shoe4 from './shoe4.png'
+import shoe5 from './shoe5.png'
+import shoe6 from './shoe6.png'
+import shoe7 from './shoe7.png'
+import shoe8 from './shoe8.svg'
+
+import thumbnailBackground from './thumbnail-background.svg'
+
+export {
+    bigShoe1,
+    bigShoe2,
+    bigShoe3,
+
+    customer1,
+    customer2,
+
+    footerLogo,
+    headerLogo,
+
+    offer,
+    shoe4,
+    shoe5,
+    shoe6,
+    shoe7,
+    shoe8,
+
+    thumbnailBackground
+}
---
+++ src/constants/index.js
@@ -0,0 +1,124 @@
+import { facebook, instagram, shieldTick, support, truckFast, twitter } from "../assets/icons";
+import { bigShoe1, bigShoe2, bigShoe3, customer1, customer2, shoe4, shoe5, shoe6, shoe7 } from "../assets/images";
+
+export const navLinks = [
+    { href: "#home", label: "Home" },
+    { href: "#about-us", label: "About Us" },
+    { href: "#products", label: "Products" },
+    { href: "#contact-us", label: "Contact Us" },
+];
+
+export const shoes = [
+    {
+        thumbnail: bigShoe1,
+        bigShoe: bigShoe1,
+    },
+    {
+        thumbnail: bigShoe2,
+        bigShoe: bigShoe2,
+    },
+    {
+        thumbnail: bigShoe3,
+        bigShoe: bigShoe3,
+    },
+];
+
+export const statistics = [
+    { value: '1k+', label: 'Brands' },
+    { value: '500+', label: 'Shops' },
+    { value: '250k+', label: 'Customers' },
+];
+
+export const products = [
+    {
+        imgURL: shoe4,
+        name: "Puma RS-X Toys",
+        price: "$200.20",
+    },
+    {
+        imgURL: shoe5,
+        name: "Mercedes AMG Patronas",
+        price: "$210.20",
+    },
+    {
+        imgURL: shoe6,
+        name: "Puma PETRONAS RS-X ",
+        price: "$220.20",
+    },
+    {
+        imgURL: shoe7,
+        name: "Puma Softride Pro",
+        price: "$230.20",
+    },
+];
+
+export const services = [
+    {
+        imgURL: truckFast,
+        label: "Free shipping",
+        subtext: "Enjoy seamless shopping with our complimentary shipping service."
+    },
+    {
+        imgURL: shieldTick,
+        label: "Secure Payment",
+        subtext: "Experience worry-free transactions with our secure payment options."
+    },
+    {
+        imgURL: support,
+        label: "Love to help you",
+        subtext: "Our dedicated team is here to assist you every step of the way."
+    },
+];
+
+export const reviews = [
+    {
+        imgURL: customer1,
+        customerName: 'Morich Brown',
+        rating: 4.5,
+        feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
+    },
+    {
+        imgURL: customer2,
+        customerName: 'Lota Mongeskar',
+        rating: 4.5,
+        feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
+    }
+];
+
+
+export const footerLinks = [
+    {
+        title: "Products",
+        links: [
+            { name: "AMG Patronas", link: "/" },
+            { name: "Ferrari Edition", link: "/" },
+            { name: "RS-X", link: "/" },
+            { name: "Puma Softride", link: "/" },
+            { name: "BMW M Motorsport", link: "/" },
+            { name: "Scuderia Ferrari", link: "/" },
+        ],
+    },
+    {
+        title: "Help",
+        links: [
+            { name: "About us", link: "/" },
+            { name: "FAQs", link: "/" },
+            { name: "How it works", link: "/" },
+            { name: "Privacy policy", link: "/" },
+            { name: "Payment policy", link: "/" },
+        ],
+    },
+    {
+        title: "Get in touch",
+        links: [
+            { name: "customer@puma.com", link: "mailto:customer@puma.com" },
+            { name: "+92554862354", link: "tel:+92554862354" },
+        ],
+    },
+];
+
+export const socialMedia = [
+    { src: facebook, alt: "facebook logo" },
+    { src: twitter, alt: "twitter logo" },
+    { src: instagram, alt: "instagram logo" },
+];
